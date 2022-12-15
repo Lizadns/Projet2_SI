940841d31c3e7d75808d788cd2a14f0436183fb1
Albane <denisalbane@gmail.com>
2022-12-15 18:57:37 +0100

    correction fontion check archive

--- Projet2_SI/lib_tar.c
+++ Projet2_SI/lib_tar.c
@@ -28,7 +28,7 @@ int check_archive(int tar_fd) {
     size_t size = 512;
     char zeroBlock[size*2];
     memset(zeroBlock, 0, size*2);//rempli zeroblock de 512*2 zéros
-    uintptr_t size_files = 0;//pour avancer du bon nombre de bits pour voir le prochain header ou la fin du fichier
+    int size_files = 0;//pour avancer du bon nombre de bits pour voir le prochain header ou la fin du fichier
     while (1){
         pread(tar_fd, &verif_end, size*2, nb_headers*size+size_files);
         if (memcmp(verif_end,zeroBlock, size*2)==0){ //regarde si c'est la fin du fichier en comparant avec la fin théorique
@@ -46,10 +46,10 @@ int check_archive(int tar_fd) {
         for (int i =0; i < 64; i++){
             verif_chksum += *(ptr+i);
         }
-        if (memcmp(&verif_chksum, header.chksum,8)!=0){
+        if (verif_chksum != TAR_INT(header.chksum)){
             return -3;
         }
-        size_files += (uintptr_t) header.size;
+        size_files += TAR_INT(header.size);
         nb_headers ++;
 
     }
